4b64a759e59c002de58f62ec8b8dc11c2effd5e6
zhaoyuheqing <proooprop@outlook.com>
2025-12-16 09:02:41 +0800

    Update build.gradle.kts

--- V2rayNG/app/build.gradle.kts
+++ V2rayNG/app/build.gradle.kts
@@ -5,11 +5,11 @@ plugins {
 }
 
 android {
-    namespace = "com.v2ray.ang"
+    namespace = "com.example.networktool"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.v2ray.ang"
+        applicationId = "com.example.networktool"
         minSdk = 21
         targetSdk = 35
         versionCode = 683
